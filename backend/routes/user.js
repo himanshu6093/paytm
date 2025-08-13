cc4cd64337207a9254ad42671986550cea932d65
himanshu6093 <himanshukumarmahto93@gmail.com>
2025-08-13 16:20:22 +0530

    signup route added

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,55 @@
 const express = require("express");
-
+const userModel = require("../db")
 const router = express.Router();
+const zod = require("zod");
+const {JWT_SECRET} =require("../config")
 
-router.get("/", (req, res) => {
-    
+const signupSchema = zod.object({
+    username: zod.string().email(),
+    password: zod.string(),
+    firstName: zod.string(),
+    lastName: zod.string()
+})
+router.post("/signup", async(req, res) => {
+    const body = req.body;
+    const {success} = signupSchema.safeParse(body);
+    if(!success) {
+        return res.json({
+            message: "email already taken/ incorrect input"
+        })
+    }
+
+    const existingUser = userModel.findOne({
+        username: req.body.username
+    })
+
+    if(existingUser) {
+        return res.status(404).json({
+            messgae: "user already signed up!!"
+        })
+    } 
+
+    const user = await userModel.create({
+        username: req.body.username,
+        password: req.body.password,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName
+    })
+
+    const userId = user._id;
+
+    const token = jwt.sign({
+        userId
+    }, JWT_SECRET)
+
+    res.json({
+        message: "user created successfully!!",
+        token: token
+    })
+})
+
+router.post("/signin", (req, res) => {
+
 })
 
 module.exports = router;
